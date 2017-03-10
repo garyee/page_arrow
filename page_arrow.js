453ceedf57da122e858c208cca60feb2cb81704f
garyee <garyee@gmx.de>
2017-03-10 10:01:28 +0100

    better Classes and disable Click-Listener

--- page_arrow.js
+++ page_arrow.js
@@ -1,92 +1,96 @@
 $.fn.dataTableExt.oPagination.page_arrows = {
-        "fnInit": function ( oSettings, nPaging, fnCallbackDraw )
-        {
-            var counter = document.createElement('div');
-            var currentSpan = document.createElement( 'span' );
-            var dividerSpan = document.createElement( 'span' );
-            var totalSpan = document.createElement( 'span' );
-
-            var nPrevious = document.createElement( 'a' );
-            var nNext = document.createElement( 'a' );
-
-            nPrevious.appendChild( document.createTextNode( oSettings.oLanguage.oPaginate.sPrevious ) );
-            nNext.appendChild( document.createTextNode( oSettings.oLanguage.oPaginate.sNext ) );
-
-            currentSpan.appendChild( document.createTextNode( this.calcCurrentPage(oSettings)));
-            totalSpan.appendChild( document.createTextNode( this.calcPages(oSettings)));
-
-            var default_devider="/";
-            if(oSettings.oLanguage.oPaginate.sDivider!=undefined && oSettings.oLanguage.oPaginate.sDivider!="")
-                default_devider= oSettings.oLanguage.oPaginate.sDivider;
-
-            dividerSpan.appendChild( document.createTextNode( default_devider ));
-
-
-            dividerSpan.className = "divider";
-            currentSpan.className = "current_page";
-            totalSpan.className   = "total_page";
-
-            nPrevious.className = "paginate_button previous";
-            nNext.className="paginate_button next";
-
-            nPaging.appendChild( nPrevious );
-            nPaging.appendChild( currentSpan );
-            nPaging.appendChild( dividerSpan );
-            nPaging.appendChild( totalSpan );
-            nPaging.appendChild( nNext );
-
-            $(nPrevious).click( function() {
-                oSettings.oApi._fnPageChange( oSettings, "previous" );
-                fnCallbackDraw( oSettings );
-            } );
-
-            $(nNext).click( function() {
-                oSettings.oApi._fnPageChange( oSettings, "next" );
-                fnCallbackDraw( oSettings );
-            } );
-        },
-
-        "fnUpdate": function ( oSettings, fnCallbackDraw )
-        {
-            if ( !oSettings.aanFeatures.p )
+            "fnInit": function ( oSettings, nPaging, fnCallbackDraw )
             {
-                return;
-            }
+                var counter = document.createElement('div');
+                var currentSpan = document.createElement( 'span' );
+                var dividerSpan = document.createElement( 'span' );
+                var totalSpan = document.createElement( 'span' );
 
-            /* Loop over each instance of the pager */
-            var an = oSettings.aanFeatures.p;
+                var nPrevious = document.createElement( 'a' );
+                var nNext = document.createElement( 'a' );
 
-            for ( var i=0, iLen=an.length ; i<iLen ; i++ )
+                nPrevious.appendChild( document.createTextNode( oSettings.oLanguage.oPaginate.sPrevious ) );
+                nNext.appendChild( document.createTextNode( oSettings.oLanguage.oPaginate.sNext ) );
+
+                currentSpan.appendChild( document.createTextNode( this.calcCurrentPage(oSettings)));
+                totalSpan.appendChild( document.createTextNode( this.calcPages(oSettings)));
+
+                var default_devider="/";
+                if(oSettings.oLanguage.oPaginate.sDivider!=undefined && oSettings.oLanguage.oPaginate.sDivider!="")
+                    default_devider= oSettings.oLanguage.oPaginate.sDivider;
+
+                dividerSpan.appendChild( document.createTextNode( default_devider ));
+
+
+                dividerSpan.className = "divider";
+                currentSpan.className = "current_page";
+                totalSpan.className   = "total_page";
+
+                nPrevious.className = "paginate_arrow previous";
+                nNext.className="paginate_arrow next";
+
+                nPaging.appendChild( nPrevious );
+                nPaging.appendChild( currentSpan );
+                nPaging.appendChild( dividerSpan );
+                nPaging.appendChild( totalSpan );
+                nPaging.appendChild( nNext );
+
+                $(nPrevious).click( function() {
+                    if (!$(this).hasClass("disabled")) {
+                        oSettings.oApi._fnPageChange(oSettings, "previous");
+                        fnCallbackDraw(oSettings);
+                    }
+                } );
+
+                $(nNext).click( function() {
+                    if (!$(this).hasClass("disabled")) {
+                        oSettings.oApi._fnPageChange(oSettings, "next");
+                        fnCallbackDraw(oSettings);
+                    }
+                } );
+            },
+
+            "fnUpdate": function ( oSettings, fnCallbackDraw )
             {
-                var buttons = an[i].getElementsByTagName('a');
-                if ( oSettings._iDisplayStart === 0 )
+                if ( !oSettings.aanFeatures.p )
                 {
-                    buttons[0].className = "paginate_disabled_previous";
+                    return;
                 }
-                else
+
+                /* Loop over each instance of the pager */
+                var an = oSettings.aanFeatures.p;
+
+                for ( var i=0, iLen=an.length ; i<iLen ; i++ )
                 {
-                    buttons[0].className = "paginate_enabled_previous";
-                }
-                if ( oSettings.fnDisplayEnd() == oSettings.fnRecordsDisplay() )
-                {
-                    buttons[1].className = "paginate_disabled_next";
-                }
-                else
-                {
-                    buttons[1].className = "paginate_enabled_next";
-                }
-                var spans = an[i].getElementsByTagName('span');
+                    var buttons = an[i].getElementsByTagName('a');
+                    if ( oSettings._iDisplayStart === 0 )
+                    {
+                        buttons[0].className = "paginate_arrow previous disabled";
+                    }
+                    else
+                    {
+                        buttons[0].className = "paginate_arrow previous";
+                    }
+                    if ( oSettings.fnDisplayEnd() == oSettings.fnRecordsDisplay() )
+                    {
+                        buttons[1].className = "paginate_arrow next disabled";
+                    }
+                    else
+                    {
+                        buttons[1].className = "paginate_arrow next";
+                    }
+                    var spans = an[i].getElementsByTagName('span');
                     $(spans[0]).html(this.calcCurrentPage(oSettings));
-                $(spans[2]).html(this.calcPages(oSettings));
+                    $(spans[2]).html(this.calcPages(oSettings));
 
 
 
+                }
+            },
+            "calcCurrentPage": function(oSettings) {
+                return Math.ceil(oSettings._iDisplayStart / oSettings._iDisplayLength) + 1;
+            },
+            "calcPages": function(oSettings) {
+                return Math.ceil(oSettings.fnRecordsDisplay() / oSettings._iDisplayLength);
             }
-        },
-        "calcCurrentPage": function(oSettings) {
-            return Math.ceil(oSettings._iDisplayStart / oSettings._iDisplayLength) + 1;
-        },
-        "calcPages": function(oSettings) {
-            return Math.ceil(oSettings.fnRecordsDisplay() / oSettings._iDisplayLength);
-        }
-    };
+        };
